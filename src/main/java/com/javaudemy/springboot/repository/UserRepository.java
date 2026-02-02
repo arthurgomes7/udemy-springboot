664e511e08b11f024d0af705a7219a00539e57e8
Arthur Gomes <arthurszaa7@icloud.com>
2026-02-02 00:46:15 -0300

    new: Implementando Services e Registro de Componentes

--- src/main/java/com/javaudemy/springboot/repository/UserRepository.java
+++ src/main/java/com/javaudemy/springboot/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.javaudemy.springboot.repository;
 
 import com.javaudemy.springboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> { // user -> entidade / Long - tipo do ID
 
 }
