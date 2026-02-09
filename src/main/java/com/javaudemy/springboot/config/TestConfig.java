4286d67092c7c500a560ce85245ff89c4b992209
Arthur Gomes <arthurszaa7@icloud.com>
2026-02-09 20:56:19 -0300

    new: Implementando Entidade Payment

--- src/main/java/com/javaudemy/springboot/config/TestConfig.java
+++ src/main/java/com/javaudemy/springboot/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Date;
 
 @Configuration
 @Profile("test")
@@ -69,6 +70,9 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
 
+        orderRepository.save(o1);
     }
 }
