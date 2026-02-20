9b5f4d81da838b6826b227fad206797d6eb5328f
Arthur Gomes <arthurszaa7@icloud.com>
2026-02-20 00:23:31 -0300

    new: Implementando Exceção para findById

--- src/main/java/com/javaudemy/springboot/services/UserService.java
+++ src/main/java/com/javaudemy/springboot/services/UserService.java
@@ -2,6 +2,7 @@ package com.javaudemy.springboot.services;
 
 import com.javaudemy.springboot.entities.User;
 import com.javaudemy.springboot.repository.UserRepository;
+import com.javaudemy.springboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
