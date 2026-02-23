28c45cc609fa874cd8c9f39abec47ff24119546a
Arthur Gomes <arthurszaa7@icloud.com>
2026-02-22 23:53:47 -0300

    new: Implementado Exceção Delete

--- src/main/java/com/javaudemy/springboot/services/UserService.java
+++ src/main/java/com/javaudemy/springboot/services/UserService.java
@@ -2,8 +2,11 @@ package com.javaudemy.springboot.services;
 
 import com.javaudemy.springboot.entities.User;
 import com.javaudemy.springboot.repository.UserRepository;
+import com.javaudemy.springboot.services.exceptions.DatabaseException;
 import com.javaudemy.springboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -30,7 +33,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
